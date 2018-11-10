a21bff66f2268094f6587248cd4d87ff54eec349
meq <fewell@gmail.com>
2018-11-10 16:51:36 -0500

    added test

--- test/ship_test.js
+++ test/ship_test.js
@@ -4,6 +4,13 @@ describe('Check for ship', function(){
     let checkForShip = require('../game_logic/ship_methods').checkForShip
 
     it('should correctly report no ship at a given players coordinate', function (){
+        player = {
+            ships: [
+                {
+                    locations: [[0,0]]
+                }
+            ]
+        }
         expect(checkForShip(player, [9,9])).to.be.false
     })
 })
