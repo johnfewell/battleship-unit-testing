ec6413ca466b8f181eecceacf7be19b27045c629
meq <fewell@gmail.com>
2018-11-10 16:58:28 -0500

    checkForShip is now passing

--- game_logic/ship_methods.js
+++ game_logic/ship_methods.js
@@ -1,5 +1,14 @@
-function checkForShip(){
+function checkForShip(player, coordinates){
+    let shipPresent
 
+    for (let ship of player.ships ){
+        shipPresent = ship.locations.filter(function(actualCoordinate){
+            return (actualCoordinate[0] === coordinates[0]) && (actualCoordinate[1] === coordinates[1])
+        })[0]
+    if (!shipPresent){ 
+        return false 
+    }
+    }
 }
 
 module.exports.checkForShip = checkForShip
