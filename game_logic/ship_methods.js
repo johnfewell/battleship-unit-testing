4d0befc7b281036dc82ae4f786bac531dcf5edad
meq <fewell@gmail.com>
2018-11-10 17:01:49 -0500

    updated checkForShip to return true

--- game_logic/ship_methods.js
+++ game_logic/ship_methods.js
@@ -1,14 +1,19 @@
-function checkForShip(player, coordinates){
-    let shipPresent
+function checkForShip(player, coordinates) {
+  let shipPresent;
 
-    for (let ship of player.ships ){
-        shipPresent = ship.locations.filter(function(actualCoordinate){
-            return (actualCoordinate[0] === coordinates[0]) && (actualCoordinate[1] === coordinates[1])
-        })[0]
-    if (!shipPresent){ 
-        return false 
+  for (let ship of player.ships) {
+    shipPresent = ship.locations.filter(function(actualCoordinate) {
+      return (
+        actualCoordinate[0] === coordinates[0] &&
+        actualCoordinate[1] === coordinates[1]
+      );
+    })[0];
+    if (!shipPresent) {
+      return false;
+    } else {
+      return true;
     }
-    }
+  }
 }
 
-module.exports.checkForShip = checkForShip
+module.exports.checkForShip = checkForShip;
