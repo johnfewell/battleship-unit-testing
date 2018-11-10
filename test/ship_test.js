0170ab7ced476cbf4638277ac5a2ffca112137d7
meq <fewell@gmail.com>
2018-11-10 16:43:47 -0500

    added test for ship_method, added game_logic file

---
+++ test/ship_test.js
@@ -0,0 +1,9 @@
+const expect = require("chai").expect;
+
+describe('Check for ship', function(){
+    let checkForShip = require('../game_logic/ship_methods').checkForShip
+
+    it('should correctly report no ship at a given players coordinate', function (){
+
+    })
+})
